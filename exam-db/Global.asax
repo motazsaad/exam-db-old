4285901e9c67016c823f9a9be3d5b897e9bd0a5e
omar kishko <omarskishko@gmail.com>
2019-03-09 15:20:53 +0200

    Add project files.

---
+++ exam-db/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="exam_db.MvcApplication" Language="C#" %>
---
+++ exam-db/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(exam_db.Startup))]
+namespace exam_db
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
