bc35f7122b5e5300178b280292b3ddd8f97c9edb
Omar Kishko <omarskishko>
2019-03-11 04:36:18 +0200

    sign up page done

--- exam-db/Scripts/site.js
+++ exam-db/Scripts/site.js
@@ -1,34 +1,14 @@
 ﻿
 $(window).on('load', function () {
-
-    $('.login_form').find('input:not([type="submit"]), textarea').on('load', function (e) {
-
-        var $this = $(this),
-            label = $this.prev('label');
-
-        if ($this.val() === '') {
-            $(this).css({ "height": "44px", "padding": "5px 12px" });
-
-            label.removeClass('active highlight');
-        } else {
-            $(this).css({ "height": "65px", "padding": "20px 12px 5px 12px" });
-            label.addClass('active highlight');
-        }
     
-
-    });
-
-
     // animate login & signup fields placeholder
-    $('.login_form').find('input:not([type="submit"]), textarea').on('keyup  blur focus', function (e) {
+    $('.login_form, .signup_form').find('input:not([type="submit"]), textarea').on('keyup  blur focus', function (e) {
 
         var $this = $(this),
             label = $this.prev('label');
-
         if (e.type === 'keyup' || e.type === 'keydown') {
             if ($this.val() === '') {
                 $(this).css({ "height": "44px", "padding": "5px 12px" });
-
                 label.removeClass('active highlight');
             } else {
                 $(this).css({ "height": "65px", "padding": "20px 12px 5px 12px" });
@@ -37,14 +17,12 @@ $(window).on('load', function () {
         } else if (e.type === 'blur') {
             if ($this.val() === '') {
                 $(this).css({ "height": "44px", "padding": "5px 12px" });
-
                 label.removeClass('active highlight');
             } else {
                 $(this).css({ "height": "65px", "padding": "20px 12px 5px 12px" });
                 label.removeClass('highlight');
             }
         } else if (e.type === 'focus') {
-
             if ($this.val() === '') {
                 $(this).css({ "height": "44px", "padding": "5px 12px" });
                 label.removeClass('highlight');
@@ -54,7 +32,19 @@ $(window).on('load', function () {
                 label.addClass('highlight');
             }
         }
+    });
 
+    // animate login & signup fields placeholder (fix for reload) 
+    $('.login_form, .signup_form').find('input:not([type="submit"]), textarea').each(function () {
+        var $this = $(this),
+            label = $this.prev('label');
+        if ($this.val() === '') {
+            $(this).css({ "height": "44px", "padding": "5px 12px" });
+            label.removeClass('active highlight');
+        } else {
+            $(this).css({ "height": "65px", "padding": "20px 12px 5px 12px" });
+            label.addClass('active highlight');
+        }
     });
 
    
