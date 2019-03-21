de7bdb347c15d766bacf2c03b92ed2701333fe62
yousefhamad <y.y.h1997@hotmail.com>
2019-03-21 13:29:18 +0200

    edit file table and model

--- exam-db/Models/File.cs
+++ exam-db/Models/File.cs
@@ -24,6 +24,8 @@ namespace exam_db.Models
         public int dislike_number { get; set; }
         public int Download_number { get; set; }
         public int view_numbre { get; set; }
+        public int year { get; set; }
+        public int semester { get; set; }
         
     }
 }
