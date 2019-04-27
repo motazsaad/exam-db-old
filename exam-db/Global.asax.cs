c5cd17568e648deca14a23fc45b35edbe0ec54b8
omar kishko <omarskishko@gmail.com>
2019-04-27 17:21:30 +0300

    Courses page - ( front end )

--- exam-db/Global.asax.cs
+++ exam-db/Global.asax.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+
+using exam_db.Models;
 
 namespace exam_db
 {
@@ -16,6 +19,8 @@ namespace exam_db
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            Database.SetInitializer<ApplicationDbContext>(new DropCreateDatabaseIfModelChanges<ApplicationDbContext>());
         }
     }
 }
